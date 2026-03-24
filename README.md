47d5013924069062ab66f11bd7c5c68e72813dd4
cedrchen <cedrchen@iu.edu>
2026-03-24 16:39:06 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Fundamental_groups
+## Acknowledgments / Citation
+
+This project uses or builds upon work from:
+
+@inproceedings{piano_SVSep_2024,
+  title={Cluster and Separate: a GNN Approach to Voice and Staff Prediction for Score Engraving},
+  author={Foscarin, Francesco and Karystinaios, Emmanouil and Nakamura, Eita and Widmer, Gerhard},
+  booktitle={Proceedings of the International Society for Music Information Retrieval Conference (ISMIR)},
+  year={2024}
+}
+
+
+This program calculates the fundamental group that are played on MIDI instruments, as long as you are connected to a MIDI device
---
+++ pi_1.py
@@ -0,0 +1,51 @@
+
+# get greek labels according to the length needed
+def greek_labels(n):
+    base = [chr(i) for i in range(0x03B1, 0x03C9 + 1)]
+    labels = []
+    if n > 24:
+        for i in range(n):
+            letter = base[i % 24]
+            number = i // 24 + 1
+            labels.append(f"{letter}{number}")
+    else:
+        for i in range(n):
+            labels.append(base[i])
+    return labels
+
+# check if each element has same length
+def is_uniform(li):
+    return all(len(li[0]) == len(t) for t in li) if li else True
+
+# pi_1
+def individual_pi_1(li, dim):
+    if is_uniform:
+        a_1=[]
+        a_2=[]
+
+        if li[-1] != li[0]:
+            li.append(li[0])         # making sure start == end
+
+        dimension = len(li[0])
+
+        a_2.append(greek_labels(dimension))
+        
+        for i in range((len(li)-1)):
+            for u in range(dimension):
+                a_1.append((li[i+1][u] - li[i][u]) // dim)         # checks the n and n+1 element and store them in an array
+            a_2.append(a_1)
+            a_1 = []
+            # store the strcutre generated into an 2 dimensional array
+            # each element represents structure generated
+            # for instance a_2[x][0] are all the betas, a_2[x][0] are all the alphas
+        return a_2
+
+# get total structure
+# only take inputs after run by individual_pi_1
+def total_pi_1(li):
+    transposed = [list(row) for row in zip(*li)]
+    for n, i in enumerate(transposed):
+        transposed[n][1] = sum(i[1:])
+        transposed[n] = transposed[n][0:2]
+    return transposed
+
